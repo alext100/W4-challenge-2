fd34a81ae7acc1b91c719a7ba0d14966a2bb9b4d
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-10-19 21:42:55 +0200

    hangmanbox class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import AnswerBox from "./components/AnswerBox";
+import HangManBox from "./components/HangManBox";
 import Letter from "./components/Letter";
 
 function App() {
   return (
     <>
+      <HangManBox></HangManBox>
       <Letter letter={"S"} />
       <AnswerBox letter={"S"} worldLength={10} />
     </>
